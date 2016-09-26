68a160f35324d4eae346db970f594b01ad417485
Alexey Zatsepin <alexzatsepin7@gmail.com>
2016-09-26 23:54:30 +0300

    update Wolfram

--- src/main/java/com/pavel/alex/lab/first/Main.java
+++ src/main/java/com/pavel/alex/lab/first/Main.java
@@ -1,28 +1,32 @@
 package com.pavel.alex.lab.first;
 
 
+import com.pavel.alex.lab.first.generator.EricWolfram;
 import com.pavel.alex.lab.first.generator.Generator;
-import com.pavel.alex.lab.first.generator.LehmerHigh;
+import com.pavel.alex.lab.first.test.StatisticalEquitabilityTest;
+import com.pavel.alex.lab.first.test.StatisticalSignIndependencyTest;
+import com.pavel.alex.lab.first.test.StatisticalTest;
+import com.pavel.alex.lab.first.test.StatisticalUniformityTest;
 
 
 public class Main {
     public static void main(String[] args) {
 
 
-        Generator generator = new LehmerHigh();
+        Generator generator = new EricWolfram();
 
-        for(int i=0;i<100;i++){
-            System.out.println(generator.generateNext());
-        }
+//        for(int i=0;i<100;i++){
+//            System.out.println(generator.generateNext());
+//        }
 
-//        StatisticalTest test = new StatisticalSignIndependencyTest(generator,1000,0.05);
-//        test.test();
-//
-//        test = new StatisticalEquitabilityTest(generator,10000,0.05);
-//        test.test();
-//
-//        test = new StatisticalUniformityTest(generator,10000,0.05,100);
-//        test.test();
+        StatisticalTest test = new StatisticalSignIndependencyTest(generator,1000,0.05);
+        test.test();
+
+        test = new StatisticalEquitabilityTest(generator,10000,0.05);
+        test.test();
+
+        test = new StatisticalUniformityTest(generator,10000,0.05,100);
+        test.test();
 
     }
 
